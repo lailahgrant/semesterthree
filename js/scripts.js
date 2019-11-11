bf072ac3bf0106d969cdccbd0ce6adb375d24f9f
lailahgrant <lailahgrant19@gmail.com>
2019-11-11 12:39:20 +0300

    today I pushed my 3rd semester project

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//jQuery toggle() effect with speed parameter
+$(document).ready(function(){  
+	$(".c1").hide();
+     $("#toggle1").click(function(){  
+        $(".c1").toggle(1500);
+        $("#toggle1").hide();  
+    }); 
+});  
+
+
+//jQuery toggle() effect with speed parameter
+$(document).ready(function(){  
+	$(".c").hide();
+     $("#toggle").click(function(){  
+        $(".c").toggle(1500);
+        $("#toggle").hide();  
+    }); 
+}); 
